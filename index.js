77b60499fd514b96627e4a00703da9c2bf027a0a
Leonardo <l.brognoli93@gmail.com>
2019-05-15 19:56:11 -0300

    some changes

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const ECB = require('./ECBLastPrice')
 
 const app = express()
 
+app.get("/", (req, res) => {
+    res.send(`<h3>To use the tool go to https://ecblastprices.herokuapp.com/ECB/lastPrice?curr=BRL, for example.</h3> 
+                <p>You can change the currency code in the parameter curr in order to see other currencies</p>`)
+})
+
 app.get("/ECB/lastPrice", (req, res) => {
     ECB.getLastPrice(req.query.curr).then(r => res.send(r) ).catch(e => res.send({
         "error" : "Something went very wrong"
